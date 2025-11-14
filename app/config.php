0d936c41c410eee97d04f29366e68232427b5eb1
qarbona <quentin.arbona@gmail.com>
2025-11-14 16:07:48 +0100

    Suite à la notation de la partie FrontEnd de l'ECF, j'ai corrigé les problèmes de header et ajouté un retour à la page d'accueil sur le logo.

--- app/config.php
+++ app/config.php
@@ -7,7 +7,7 @@ $password = 'MotDePasseFort123!';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie ✅"; // décommente si tu veux tester immédiatement
+    echo "Connexion réussie ✅"; 
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
